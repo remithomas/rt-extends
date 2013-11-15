4a35619f0014aead7f4b5b1542a36d7898adacdc
Rémi THOMAS <remi.thomas@yahoo.fr>
2013-11-15 13:09:29 -0500

    Autoload_classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -9,6 +9,7 @@ return array(
     'RtExtends\Useful\Languages'                    => __DIR__ . '/src/RtExtends/Useful/Languages.php',
     'RtExtends\Useful\Countries'                    => __DIR__ . '/src/RtExtends/Useful/Countries.php',
     'RtExtends\Db\Sql\DuplicateInsert'              => __DIR__ . '/src/RtExtends/Db/Sql/DuplicateInsert.php',
+    'RtExtends\Db\Sql'                              => __DIR__ . '/src/RtExtends/Db/Sql.php',
     'RtExtends\Validator\Date\IsLater'              => __DIR__ . '/src/RtExtends/Validator/Date/IsLater.php',
     'RtExtends\Validator\Date\IsEarlier'            => __DIR__ . '/src/RtExtends/Validator/Date/IsEarlier.php',
     'RtExtends\Useful\File\Zip'                     => __DIR__ . '/src/RtExtends/Useful/File/Zip.php',
